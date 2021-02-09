cf6bc3f13d0a06375139b6e975d1bdf2477fc6d2
VVaria <2001varvara11@gmail.com>
2021-02-09 15:57:52 +0300

    Fix

--- source/letters.js
+++ source/letters.js
@@ -1,16 +1,29 @@
 'use strict';
 
+/***
+ * Count amount of one symbol in line
+ * @param {string} s - line to find amount of symbol
+ * @param {string} l - symbol to find
+ * @returns {number}
+ */
 const countLetter = (s, l) => s.split(l).length - 1;
 
-const letters = (stroke, leaveSymbol = -1) => {
+/***
+ * Delete repeating symbols in line
+ * @param {string} stroke - line
+ * @param {boolean} leaveSymbol - true if leave first of repeating symbol, false - leave last, nothing - not leave any of repeating symbols
+ * @returns {string}
+ */
+const letters = (stroke, leaveSymbol) => {
     for (let i = 0; i < stroke.length; i++) {
-        let symbol = stroke.charAt(i);
-        let amountOfLetter = countLetter(stroke, symbol);
+        const symbol = stroke[i];
+        const amountOfLetter = countLetter(stroke, symbol);
         if (amountOfLetter > 1) {
-            let place = 0;
-            leaveSymbol ? place = stroke.indexOf(symbol) : place = stroke.lastIndexOf(symbol) - amountOfLetter + 1;
+            const place = leaveSymbol ? stroke.indexOf(symbol) : stroke.lastIndexOf(symbol) - amountOfLetter + 1;
             stroke = stroke.replaceAll(symbol, '');
-            leaveSymbol !== -1 ? stroke = stroke.slice(0, place) + symbol + stroke.slice(place) : undefined;
+            if (leaveSymbol !== undefined) {
+                stroke = stroke.slice(0, place) + symbol + stroke.slice(place);
+            }
         }
     }
     return stroke;
